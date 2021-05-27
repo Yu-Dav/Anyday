1e64262df362b2aeaa708ca37c9c59e03ee0de2d
Dafna Bashan <dafnaba1@gmail.com>
2021-05-27 17:05:53 +0300

    header

--- frontend/src/cmps/BoardHeader.jsx
+++ frontend/src/cmps/BoardHeader.jsx
@@ -3,26 +3,50 @@ import ContentEditable from 'react-contenteditable'
 
 export class BoardHeader extends Component {
     state = {
-        boardTitle: 'Hello world'
+        boardTitle: 'Hello world',
+        subTitle: 'im here!'
     }
 
     constructor() {
         super()
         this.contentEditable = React.createRef();
-        this.state = { boardTitle: 'Hello world' };
+        this.state = { boardTitle: 'Hello world', subTitle: 'im here!' };
     };
 
-    handleChange = (ev) => {
-        this.setState({ boardTitle: ev.target.value });
-    };
+    // handleChange = (ev) => {
+    //     this.setState({ boardTitle: ev.target.value });
+    // };
+
+    handleChange = ({ target }) => {
+        const { name } = target
+        const value = target.value
+        this.setState({ ...this.state, [name]: value })
+    }
 
     render = () => {
-        return <ContentEditable
-            innerRef={this.contentEditable}
-            html={this.state.boardTitle} // innerHTML of the editable div
-            disabled={false}       // use true to disable editing
-            onChange={this.handleChange} // handle innerHTML change
-            tagName='div' // Use a custom HTML tag (uses a div by default)
-        />
+        return (
+            <div>
+                <ContentEditable className="board-title"
+                    name="boardTitle"
+                    innerRef={this.contentEditable}
+                    html={this.state.boardTitle} // innerHTML of the editable div
+                    disabled={false}       // use true to disable editing
+                    onChange={this.handleChange} // handle innerHTML change
+                    tagName='div' // Use a custom HTML tag (uses a div by default)
+                    style={{ width: 300 }}
+                />
+
+                {/* <ContentEditable className="board-subtitle"
+                    name="subTitle"
+                    innerRef={this.contentEditable}
+                    html={this.state.subTitle} // innerHTML of the editable div
+                    disabled={false}       // use true to disable editing
+                    onChange={this.handleChange} // handle innerHTML change
+                    tagName='div' // Use a custom HTML tag (uses a div by default)
+                    style={{ width: 300 }}
+                /> */}
+            </div>
+        )
     };
 }
+
