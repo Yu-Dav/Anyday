36dc6c78c83fc35e1898e77507006b1a50e1feb6
Yuval <dav.yuval@gmail.com>
2021-05-30 14:22:18 +0200

    favicon and btn design

--- frontend/src/cmps/BoardHeader.jsx
+++ frontend/src/cmps/BoardHeader.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 // import ContentEditable from 'react-contenteditable'
 import { EditableCmp } from './EditableCmp'
+import { ReactComponent as StarSvg } from '../assets/imgs/svg/star.svg'
 
 export class BoardHeader extends Component {
 
@@ -25,15 +26,20 @@ export class BoardHeader extends Component {
 
             <div className="board-header">
                 <div className="flex">
-                    <div className="full title-container">
-                    <EditableCmp className="title" name="title" value={board.title} updateFunc={this.onUpdateBoard} />
+                    {/* <div className="full title-container flex"> */}
+                    <div className="title-container flex">
+                        <EditableCmp className="title" name="title" value={board.title} updateFunc={this.onUpdateBoard} />
+                        <StarSvg className="" />
                     </div>
-                    <button>Last seen</button>
-                    <button>Invite / 3</button>
-                    <button>Activity</button>
+                    <div className="board-header-btns">
+
+                        <button className="btn">Last seen</button>
+                        <button className="btn">Invite / 3</button>
+                        <button className="btn">Activity</button>
+                    </div>
                 </div>
                 <div className="full subtitle-container">
-                <EditableCmp className="subtitle" name="subtitle" value={board.subtitle} updateFunc={this.onUpdateBoard} />
+                    <EditableCmp className="subtitle" name="subtitle" value={board.subtitle} updateFunc={this.onUpdateBoard} />
                 </div>
             </div>
         )
