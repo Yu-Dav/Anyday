b53493c5982d0645da5fc180d9bf0979ce063a46
Noga Jacobi <nogajacobi@gmail.com>
2021-05-30 22:29:33 +0200

    tags

---
+++ frontend/src/cmps/Tasks/CellTagold.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+
+export class CellTag2 extends Component {
+    state = {
+        isExpanded: false
+    }
+
+    handleUpdate = ({ target }) => {
+        console.log(target)
+        // const selectedTag = this.getTagById(target.dataset.tag)
+        console.log('selectedtag',selectedTag)
+        this.props.task.tags.unshift(selectedTag) 
+        const newBoard = { ...this.props.board }
+        this.props.updateBoard(newBoard) //updating the entire board
+    }
+
+    getTagById= (tagId) => {
+        const availableTags = this.props.board.tags
+        return availableTags.find(tag => tag.id === tagId)
+    }
+
+    onOpenSelector = () => {
+        this.setState({ ...this.state, isExpanded: !this.state.isExpanded })
+    }
+
+    render() {
+        const { tags } = this.props.task
+        const { isExpanded } = this.state
+        const availableTags = this.props.board.tags
+        // console.log('available tags', availableTags)
+        return (
+            <div className="cell tags" >
+                {tags.map((tag) => {
+                    return <p style={{ color: tag.color }} key={tag.id} onClick={this.onOpenSelector}>{tag.title} </p>
+                })}
+                {isExpanded &&
+                    <div className="tag-options">
+                        {availableTags.map((tag) => {
+                            return <div className="tag-option" onClick={this.handleUpdate}
+                                key={tag.id} data-tag={tag.id} style={{ color: tag.color }}>
+                                {tag.title}
+
+                            </div>
+                        })}
+                    </div>
+                }
+            </div>
+        )
+    }
+}
