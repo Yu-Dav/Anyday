f9966ac6fe319446ba6caf12888388ed575f7c4a
Dafna Bashan <dafnaba1@gmail.com>
2021-06-06 13:27:30 +0300

    signup

--- frontend/src/cmps/Tasks/TaskPreview.jsx
+++ frontend/src/cmps/Tasks/TaskPreview.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { CellTitle } from './CellTitle'
-import { CellMember } from './CellMember'
-import { CellTag } from './CellTag'
-import { CellStatus } from './CellStatus'
-import { CellPriority } from './CellPriority'
-import { CellCreationLog } from './CellCreationLog'
-import { CellDate } from './CellDate'
+// import { CellTitle } from './CellTitle'
+// import { CellMember } from './CellMember'
+// import { CellTag } from './CellTag'
+// import { CellStatus } from './CellStatus'
+// import { CellPriority } from './CellPriority'
+// import { CellCreationLog } from './CellCreationLog'
+// import { CellDate } from './CellDate'
 import { Draggable } from 'react-beautiful-dnd';
+import {DynamicCell} from './DynamicCell'
 
 
 export function TaskPreview({ task, group, board, updateBoard, index }) {
-    const cellTypes = ['title', 'member', 'tag', 'status', 'priority', 'creationLog', 'timeline', 'location']
+    // const cellTypes = ['title', 'member', 'tag', 'status', 'priority', 'creationLog', 'timeline', 'location']
+    console.log(board.cellTypes);
     return (
         <React.Fragment>
             <Draggable draggableId={task.id} index={index} type="task">
@@ -19,16 +21,17 @@ export function TaskPreview({ task, group, board, updateBoard, index }) {
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
                         ref={provided.innerRef}>
-                        <span className="left-container flex">
+                        {/* <span className="left-container flex"> */}
                             <div className="group-color" style={{ backgroundColor: group.style.bgColor }}></div>
-                            <CellTitle task={task} group={group} board={board} updateBoard={updateBoard} />
-                        </span>
-                        <CellMember task={task} group={group} board={board} updateBoard={updateBoard} />
+                            {/* <CellTitle task={task} group={group} board={board} updateBoard={updateBoard} /> */}
+                        {/* </span> */}
+                        {board.cellTypes.map(cellType => <DynamicCell type={cellType} task={task} group={group} board={board} updateBoard={updateBoard}/>)}
+                        {/* <CellMember task={task} group={group} board={board} updateBoard={updateBoard} />
                         <CellTag task={task} group={group} board={board} updateBoard={updateBoard} />
                         <CellStatus task={task} group={group} board={board} updateBoard={updateBoard} />
                         <CellPriority task={task} group={group} board={board} updateBoard={updateBoard} />
                         <CellCreationLog task={task} />
-                        <CellDate task={task} group={group} board={board} updateBoard={updateBoard} />
+                        <CellDate task={task} group={group} board={board} updateBoard={updateBoard} /> */}
                     </div>
                 )}
             </Draggable>
@@ -36,3 +39,30 @@ export function TaskPreview({ task, group, board, updateBoard, index }) {
     )
 }
 
+//old task preview
+// export function TaskPreview({ task, group, board, updateBoard, index }) {
+//     const cellTypes = ['title', 'member', 'tag', 'status', 'priority', 'creationLog', 'timeline', 'location']
+//     return (
+//         <React.Fragment>
+//             <Draggable draggableId={task.id} index={index} type="task">
+//                 {provided => (
+//                     <div className="task-row flex"
+//                         {...provided.draggableProps}
+//                         {...provided.dragHandleProps}
+//                         ref={provided.innerRef}>
+//                         <span className="left-container flex">
+//                             <div className="group-color" style={{ backgroundColor: group.style.bgColor }}></div>
+//                             <CellTitle task={task} group={group} board={board} updateBoard={updateBoard} />
+//                         </span>
+//                         <CellMember task={task} group={group} board={board} updateBoard={updateBoard} />
+//                         <CellTag task={task} group={group} board={board} updateBoard={updateBoard} />
+//                         <CellStatus task={task} group={group} board={board} updateBoard={updateBoard} />
+//                         <CellPriority task={task} group={group} board={board} updateBoard={updateBoard} />
+//                         <CellCreationLog task={task} />
+//                         <CellDate task={task} group={group} board={board} updateBoard={updateBoard} />
+//                     </div>
+//                 )}
+//             </Draggable>
+//         </React.Fragment>
+//     )
+// }
